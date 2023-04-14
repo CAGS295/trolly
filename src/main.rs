b9bf9ea4fccb3da2d919fe976837585ccd379cd5
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-04-13 18:17:50 -0600

    pretty panics with color-eyre

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use tracing_subscriber::EnvFilter;
 use trolly::Cli;
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), ()> {
+async fn main() -> Result<(), color_eyre::Report> {
+    color_eyre::install()?;
     tracing_subscriber::fmt()
         .with_env_filter(EnvFilter::from_default_env())
         .init();
