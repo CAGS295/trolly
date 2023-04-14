13b97bb1103361130d28a0aad836e22d4d153f57
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-04-13 18:17:05 -0600

    basic logger

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 pub use clap::Parser;
+use tracing_subscriber::EnvFilter;
 use trolly::Cli;
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() {
+async fn main() -> Result<(), ()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
     Cli::parse().start().await;
+    Ok(())
 }
