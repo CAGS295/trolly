73b67cbc4021d1ffc5911f62d6fd4a22e8cc2b2e
Carlos Alejandro Gutierrez Sandoval <calgusa@gmail.com>
2023-07-27 21:38:31 -0600

    WIP opentelemetry

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 pub use clap::Parser;
+use opentelemetry::global;
 use tracing::Level;
-use tracing_subscriber::EnvFilter;
+use tracing_subscriber::{
+    fmt, prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt, EnvFilter,
+};
 use trolly::Cli;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 2)]
@@ -9,7 +12,22 @@ async fn main() -> Result<(), color_eyre::Report> {
     let filter = EnvFilter::try_from_default_env().or_else(|_| {
         Ok::<_, color_eyre::Report>(EnvFilter::default().add_directive(Level::INFO.into()))
     })?;
-    tracing_subscriber::fmt().with_env_filter(filter).init();
+
+    //tracing_subscriber::fmt().with_env_filter(filter).init();
+    global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+
+    let tracer = opentelemetry_jaeger::new_agent_pipeline()
+        .with_service_name("depth server")
+        .install_simple()?;
+
+    let otel = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    tracing_subscriber::registry()
+        .with(otel)
+        // Continue logging to stdout
+        .with(fmt::Layer::default())
+        .with(filter)
+        .try_init()?;
 
     Cli::parse().start().await;
     Ok(())
