5a158cf33a56eea002536ff0d9b64ea473c680e4
cags295 <calgusa@gmail.com>
2026-04-19 11:39:17 -0600

    deprecated criterion black_box

--- benches/depth_monitor_scale.rs
+++ benches/depth_monitor_scale.rs
@@ -1,5 +1,6 @@
 use axum::body::Body;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
 use flate2::read::GzDecoder;
 use http::header::ACCEPT_ENCODING;
 use http::Request;
